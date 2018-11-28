0ee3ec3d73e5fa863256228f407006394819b855
Developerctm <developercm@protonmail.com>
2018-11-28 03:00:43 +0300

    v.1.0.0

--- CrystalTrader/data/encrypt-keys.bat
+++
@@ -1,2 +0,0 @@
-dotnet ..\bin\Debug\netcoreapp2.1\CrystalTrader.dll --encrypt --path=keys.bin --publickey=public_key --privatekey=private_key
-pause
--- CrystalTrader/data/encrypt-keys.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dotnet bin/CrystalTrader.dll --encrypt --path=keys.bin --publickey=public_key --privatekey=private_key
